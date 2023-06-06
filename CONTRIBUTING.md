28a4b47980d62c20bafaab40dd934771f7f2ac00
tymek lewandowski <tymek@tymeks-MacBook-Air.local>
2023-06-06 17:24:50 +0100

    gosher-studios > fimsh

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -4,6 +4,6 @@ uhh so basically features that need to be done are usually in TODO
   * reporting feature
   * having a terms of service
   * mayyybe rate limiting but that seems mean
-  * copy to clipboard is broken idk why
+  * copy to clipboard is broken idk why REASON WHY NO HTTPS IS BECAUSE NEED HTTPS CERTIFICATE
   * make qr code look nicer or be a not png? puit floppa logo in middle
   * kiss boys :3 task completed
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use askama::Template;
+use async_std::stream::StreamExt;
 use async_std::{fs::OpenOptions, io};
 use nanoid::nanoid;
 use qrcode::render::svg;
@@ -14,7 +15,7 @@ pub type Result<T = ()> = std::result::Result<T, Box<dyn std::error::Error>>;
 pub struct State {
     path: PathBuf,
     url: String,
-    max: i16,
+    max: u64,
     port: String,
 }
 
@@ -49,8 +50,9 @@ pub async fn serve(req: Request<State>) -> tide::Result {
 }
 
 pub async fn upload(req: Request<State>) -> tide::Result {
+    println!("{:?}", req.len());
     match req.len() {
-        Some(size) if !(size as i16 > req.state().clone().max) => {}
+        Some(size) if !(size as u64 > req.state().clone().max) => {}
         _ => return Ok(Response::new(413)),
     };
     let path = req.param("id")?;
@@ -86,19 +88,32 @@ pub async fn qr(req: Request<State>) -> tide::Result {
 #[template(path = "home.html")]
 struct Home {
     url: String,
-    total: i8,
-    max: i16,
+    total: usize,
+    max: String,
 }
 
 pub async fn home(req: Request<State>) -> tide::Result {
-    //TODO make this less silly?
     let r = req.state().clone();
     let home: Home = Home {
         url: r.url,
-        total: 69,
-        max: r.max,
+        total: async_std::fs::read_dir("files").await?.count().await,
+        max: format_size(r.max.to_string()),
     };
     let mut body = Body::from_string(home.render()?);
     body.set_mime(Home::MIME_TYPE);
     Ok(body.into())
+}
+pub fn format_size(size: String) -> String {
+    let sizes = ["B", "KB", "MB", "GB", "TB", "PB"];
+    let mut take = size.len() % 3;
+    let mut modifier = 0;
+    if take == 0 {
+        take = 3;
+        modifier = 1;
+    }
+    format!(
+        "{}{}",
+        size.chars().take(take).collect::<String>(),
+        sizes[size.len() / 3 - modifier]
+    )
 }
