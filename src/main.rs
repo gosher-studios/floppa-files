2a985e9b4dc4b6f5f8e93ba4524d78a8127d7819
pandaroses <georgiytnimov@gmail.com>
2023-11-15 11:08:01 +0000

    added ip logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use askama::Template;
 use axum::{
     body::StreamBody,
-    extract::{BodyStream, DefaultBodyLimit, Path, State},
+    extract::{BodyStream, ConnectInfo, DefaultBodyLimit, Path, State},
     http::StatusCode,
     response::{IntoResponse, Response},
     routing::{get, put},
@@ -10,17 +10,18 @@ use axum::{
 use futures::StreamExt;
 use nanoid::nanoid;
 use qrcode::QrCode;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, net::SocketAddr, path::PathBuf, sync::Arc};
-use tokio::sync::RwLock;
 use tokio::{
     fs::File,
     io::{AsyncWriteExt, BufWriter},
+    signal,
 };
+use tokio::{io::AsyncReadExt, sync::RwLock};
 use tokio_util::io::ReaderStream;
 use tower_http::services::ServeDir;
 
-#[derive(Deserialize, Clone, Debug)]
+#[derive(Deserialize, Clone, Debug, Serialize)]
 struct AppState {
     max_size: usize,
     url: String,
@@ -28,15 +29,29 @@ struct AppState {
     listen: SocketAddr,
     #[serde(skip)]
     file_count: Arc<RwLock<usize>>,
-    // #[serde(skip)]
-    // logs: Arc<RwLock<HashMap<String, Vec<String>>>>,
+    #[serde(skip)]
+    logs: Arc<RwLock<HashMap<String, Vec<String>>>>,
 }
 
 #[tokio::main]
 async fn main() {
     let mut state: AppState =
         toml::from_str(&tokio::fs::read_to_string("config.toml").await.unwrap()).unwrap();
-    // let mut logs: HashMap<String, Vec<String>> =
+    if tokio::fs::File::open("logfile").await.is_err() {
+        let mut empty = tokio::fs::File::create("logfile").await.unwrap();
+        let mut hash: HashMap<String, Vec<String>> = HashMap::new();
+        let vecr: Vec<String> = vec!["meow.sh".to_string()];
+        hash.insert("0.0.0.0".to_string(), vecr);
+        let serialized = bincode::serialize(&hash).unwrap();
+        empty.write_all(&serialized).await.unwrap();
+        empty.shutdown().await.unwrap();
+        println!("created empty log file");
+    }
+    let mut file = tokio::fs::File::open("logfile").await.unwrap();
+    let mut output = vec![];
+    file.read_to_end(&mut output).await.unwrap();
+    let load: HashMap<String, Vec<String>> = bincode::deserialize(&output).unwrap();
+    state.logs = Arc::new(RwLock::new(load));
     if tokio::fs::create_dir(&state.file_dir).await.is_ok() {
         println!("created files directory in {:?}", &state.file_dir)
     };
@@ -50,33 +65,41 @@ async fn main() {
         .route("/qr/:id", get(qr))
         .layer(DefaultBodyLimit::max(state.max_size))
         .nest_service("/static", ServeDir::new("static"))
-        .with_state(state);
+        .with_state(state.clone().to_owned());
     println!("app initialized");
     axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .with_graceful_shutdown(shutdown())
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .with_graceful_shutdown(async {
+            signal::ctrl_c().await.unwrap();
+            println!("shutting down");
+            let mut file = File::create("logfile").await.unwrap();
+            let writer = bincode::serialize(&*state.logs.read_owned().await);
+            // file.write(&writer.unwrap()).await.unwrap();
+            file.write_all(&writer.unwrap()).await.unwrap();
+        })
         .await
         .unwrap();
 }
 
-async fn shutdown() {}
-
 async fn upload(
     Path(id): Path<String>,
     State(state): State<AppState>,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
     mut stream: BodyStream,
 ) -> Response {
-    let res: String = nanoid!(8)
-        + "."
-        + &id
-            .replace(
-                [
-                    '/', '\\', '&', '?', '"', '\'', '*', '~', '|', ':', '<', '>', ' ',
-                ],
-                "-",
-            )
-            .replace("%20", "-");
-    let path = state.file_dir.join(&res);
+    let res: Arc<String> = Arc::new(
+        nanoid!(8)
+            + "."
+            + &id
+                .replace(
+                    [
+                        '/', '\\', '&', '?', '"', '\'', '*', '~', '|', ':', '<', '>', ' ',
+                    ],
+                    "-",
+                )
+                .replace("%20", "-"),
+    );
+    let path = state.file_dir.join(Arc::clone(&res).to_string());
     let file = tokio::fs::File::create(&path).await.unwrap();
     let mut bug = BufWriter::new(file);
     while let Some(chunk) = stream.next().await {
@@ -86,8 +109,16 @@ async fn upload(
             break;
         }
     }
+    state
+        .logs
+        .write()
+        .await
+        .entry(addr.to_string())
+        .or_insert(vec![Arc::clone(&res).to_string()])
+        .push(Arc::clone(&res).to_string());
     *state.file_count.write().await += 1;
-    (StatusCode::OK, res).into_response()
+
+    (StatusCode::OK, res.to_string()).into_response()
 }
 
 async fn download(State(state): State<AppState>, Path(id): Path<String>) -> Response {
