761db813615b8ff14662f8e213a67b738feffb31
slonkazoid <alifurkanyildiz@gmail.com>
2024-01-25 02:43:21 +0300

    rewrite build.rs with version pinning

--- build.rs
+++ build.rs
@@ -1,32 +1,17 @@
 use std::process::Command;
 
+const TAILWIND_VERSION: &str = "3.4.1";
+
 fn main() {
-    if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args([
-                "/C",
-                "tailwind -i templates/main.css -o static/main.css --minify",
-            ])
-            .output()
-            .expect("failed to run tailwind, is tailwind installed????")
-    } else if cfg!(target_os = "macos") {
-        Command::new("sh")
-            .args([
-                "-c",
-                "tailwindcss -i templates/main.css -o static/main.css --minify",
-            ])
-            .output()
-            .expect("failed to run tailwind, is tailwind installed????")
-    } else {
-        Command::new("tailwind")
-            .args([
-                "-i",
-                "templates/main.css",
-                "-o",
-                "static/main.css",
-                "--minify",
-            ])
-            .output()
-            .expect("failed to run tailwind, is tailwind installed????")
-    };
+    Command::new(if cfg!(windows) { "cmd" } else { "sh" })
+        .args([
+            if cfg!(windows) { "/C" } else { "-c" },
+            format!(
+                "npx -- tailwindcss@{} -i templates/main.css -o static/main.css --minify",
+                TAILWIND_VERSION
+            )
+            .as_str(),
+        ])
+        .output()
+        .expect("failed to run tailwind, is npm installed???????");
 }
