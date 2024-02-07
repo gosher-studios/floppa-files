35f627946e4e7ebcd4a916130355acfe1221687e
chxry <chxry@floppa.world>
2024-02-07 20:13:00 +0000

    switch to xmlhttprequest

--- static/index.js
+++ static/index.js
@@ -44,47 +44,46 @@ const upload = async (files) => {
     document.getElementById("list").appendChild(log);
     createToast(msg);
 
-    let res = await fetch(`/${file.name}`, {
-      method: "PUT",
-      body: file,
-      headers: {
-        "Content-Type": "application/octet-stream",
-      },
-    });
-    // ideally we should error handle if await fetch fails buttttt :3
-    if (res.status == 200) {
-      let fileName = await res.text();
-      let url = `${location.origin}/${fileName}`;
-      createToast(`Uploaded ${fileName}`);
+    new Promise((resolve, reject) => {
+      let req = new XMLHttpRequest();
+      req.open("PUT", `/${file.name}`);
+      req.addEventListener("error", () => reject(req));
+      req.addEventListener("abort", () => reject(req));
+      req.addEventListener("load", () => (req.status === 200 ? resolve(req) : reject(req)));
+      req.send(file);
+    })
+      .then((res) => {
+        let fileName = res.responseText;
+        let url = `${location.origin}/${fileName}`;
+        createToast(`Uploaded ${fileName}`);
 
-      let newLog = document.createElement("div");
-      newLog.className = "space-x-2";
+        let newLog = document.createElement("div");
+        newLog.className = "space-x-2";
 
-      let qr = document.createElement("span");
-      qr.innerText = "qr";
-      qr.className = "underline cursor-pointer";
-      qr.onclick = () => {
-        showQr(url);
-      };
-      newLog.appendChild(qr);
+        let qr = document.createElement("span");
+        qr.innerText = "qr";
+        qr.className = "underline cursor-pointer";
+        qr.onclick = () => {
+          showQr(url);
+        };
+        newLog.appendChild(qr);
 
-      let file = document.createElement("span");
-      file.onclick = () => {
-        navigator.clipboard.writeText(url);
-        createToast("Copied to clipboard");
-      };
-      file.className = "underline cursor-pointer";
-      file.innerText = fileName;
-      newLog.appendChild(file);
+        let file = document.createElement("span");
+        file.onclick = () => {
+          navigator.clipboard.writeText(url);
+          createToast("Copied to clipboard");
+        };
+        file.className = "underline cursor-pointer";
+        file.innerText = fileName;
+        newLog.appendChild(file);
 
-      log.replaceWith(newLog);
-      fileCount++;
-      document.getElementById("total").innerText = fileCount;
-    } else {
-      let img = document.createElement("img");
-      img.src = `https://http.cat/${res.status}`;
-      log.replaceWith(img);
-    }
+        log.replaceWith(newLog);
+        fileCount++;
+        document.getElementById("total").innerText = fileCount;
+      })
+      .catch(() => {
+        log.innerText = `failed to upload ${file.name}`;
+      });
   }
 };
 
