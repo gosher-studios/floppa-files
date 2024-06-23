3a61a597755ca1c8ba403191539cabfbee7c7298
goer <georgiytnimov@gmail.com>
2024-06-23 21:15:01 +0100

    what the fuck is a pendulum

--- build.rs
+++ build.rs
@@ -1,1 +1,17 @@
-floppaplate/build.rs
+use std::fs;
+use std::process::Command;
+
+const TAILWIND_VERSION: &str = "3.4.1";
+
+fn main() {
+  Command::new(if cfg!(windows) { "cmd" } else { "sh" })
+    .args([
+      if cfg!(windows) { "/C" } else { "-c" },
+      &format!(
+        "npx -- tailwindcss@{} -i templates/main.css -o static/main.css --minify",
+        TAILWIND_VERSION
+      ),
+    ])
+    .output()
+    .expect("failed to run tailwind, is npm installed??????");
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,21 @@
-floppaplate/tailwind.config.js
+module.exports = {
+  content: ["templates/*.html"],
+  theme: {
+    colors: {
+      "bg": "#111",
+      "fg": "#202020",
+      "hover": "#0a0a0a",
+      "accent": "#aaa",
+      "white": "#faf9f6",
+      "red": "#bf616a",
+      "green": "#6abf61",
+      "grey": "#717171"
+    },
+    fontFamily: {
+      "display": ["Clash Display", "sans-serif"],
+      "sans": ["Clash Grotesk", "sans-serif"],
+      "mono": ["Roboto Mono", "monospace"],
+    },
+  },
+  plugins: [],
+}
